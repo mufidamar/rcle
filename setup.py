643c6c32eb1177c3eb87a3a58fbf96e351c6123b
mufidamar <mufidamar@gmail.com>
2019-12-10 08:23:39 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='rcle',
-	version='1.5',
+	version='1.0.0',
 	description='Install Let\'s Encrypt SSL on RunCloud servers the easy way.',
 	author="Amaruddin Mufid",
 	author_email="mufidamar@outlook.com",
