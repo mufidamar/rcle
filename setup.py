4daeca19a043c98bf286c47e67d42e8ce24874fc
mufidamar <mufidamar@gmail.com>
2019-12-12 05:40:37 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+# read the contents of your README file 
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='rcle',
+	version='1.1.0',
+	description='Install Let\'s Encrypt SSL on RunCloud servers the easy way.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+	author="Amaruddin Mufid",
+	author_email="mufidamar@outlook.com",
+	url="https://github.com/mufidamar/rcle/",
+	license="MIT",
+    packages=['rcle'],
+	entry_points={
+		'console_scripts': [
+			'rcle = rcle.rcle:main'
+		],
+	},
+	install_requires=[
+		'python-nginx'
+	],
+)
